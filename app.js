df709d9787ee5d7a5692e3f6b897ed7311b4912a
Dheeraj Manjunath <dheerajmanju1@yahoo.com>
2016-06-18 03:18:25 -0400

    redis set/get works. start of namespaces

--- app.js
+++ app.js
@@ -1,52 +1,78 @@
 //Server Side
 var express = require('express'),
-	db = require('./db'),
-	app = express(),
-	server = require('http').createServer(app),
-	io = require('socket.io').listen(server),
-	port = 3000,
-	nicknames = [],									  //to store names
-	messages = [],									  //to store messages
-	sockets = [];								     
-	
+  db = require('./db'),
+  app = express(),
+  server = require('http').createServer(app),
+  io = require('socket.io').listen(server),
+  port = 3000,
+  nicknames = [],                   //to store names
+  messages = [],                    //to store messages
+  sockets = [];                    
+  
 server.listen(port,function(){
-	console.log("Server up and running");
-	console.log("Server listening at port %d",port)
+  console.log("Server up and running");
+  console.log("Server listening at port %d",port)
 });
 
 app.get('/', function(req, res){
-	res.sendfile(__dirname + '/tchat.html');
+  res.sendfile(__dirname + '/tchat.html');
 });
 
 //on connecting do the following
-io.sockets.on('connection', function(socket){
-	socket.on('new user', function(data, callback){
-		if (nicknames.indexOf(data) != -1){           //name does not exist 
-			callback(false);
-		} else{
-			callback(true);                        
-			socket.nickname = data;                   //name exists
-			nicknames.push(socket.nickname);		  //store the name in socket itself
-			db.saveUsers(socket.nickname);
-			updateNicknames();
-		}
-	});
-	
-	function updateNicknames(){
-		io.sockets.emit('usernames', nicknames);
-	}
+var nsp = io.of('/my-namespace');
 
-	socket.on('send message', function(data){
-		io.sockets.emit('new message', {msg: data, nick: socket.nickname});
-		db.saveToDb({msg: data, name: socket.nickname});
-		db.getMessage(data);
-	});
-	
-	socket.on('disconnect', function(data){
-		if(!socket.nickname) return;                 //when user did not enter name and quit 
-		console.log(socket.nickname + " disconnected.");
-		nicknames.splice(nicknames.indexOf(socket.nickname), 1);
-		updateNicknames();
+nsp.on('connection', function(socket){
+  socket.on('new user', function(userName, callback){
+    socket.name = socket.id.split('#')[0];
 
-	});
+    socket.nickname = userName;
+    if(nicknames.indexOf(userName) === -1){
+      nicknames.push(userName);
+      updateNicknames();
+      
+
+      db.getMessage(socket.name, function(err, messages){
+        if(err){
+          throw err;
+        }
+        else{
+          if(messages){
+            messages.forEach(function(message){
+              socket.emit('new message', {msg: message.message, nick: userName});
+            });
+            callback(true);
+          }
+          else{
+            callback(true);
+          }
+        }
+      });
+    }
+    else{
+      callback(false);
+    }
+  });
+  
+  function updateNicknames(){
+    nsp.emit('usernames', nicknames);
+  }
+
+  socket.on('send message', function(data){
+    db.saveToDb(socket.name, socket.nickname, data, function(err){
+      if(err){
+        throw err;
+      }
+      else{
+        nsp.emit('new message', {msg: data, nick: socket.nickname});
+      }
+    });
+  });
+  
+  socket.on('disconnect', function(data){
+    if(!socket.nickname) return;                 //when user did not enter name and quit 
+    console.log(socket.nickname + " disconnected.");
+    nicknames.splice(nicknames.indexOf(socket.nickname), 1);
+    updateNicknames();
+
+  });
 });
--- db.js
+++ db.js
@@ -1,52 +1,68 @@
-var Redis = require('ioredis');
-var redis = new Redis(); 
+'use strict';
 
-redis.on('connect', function(err) {
-	if(err)
-		console.log('Cannot connect to DB');
-	else
-    	console.log('Connected to DB');
+var Redis = require('ioredis');
+var redis = new Redis();
+
+redis.on('connect', function(err){
+  if(err){
+    console.log('Cannot connect to DB');
+  }
+  else{
+    console.log('Connected to DB');
+  }
 });
 
-exports.saveToDb = function(data,err){
-	if(err){
-		console.log('Error in saving name:msg to DB');
-	}
-	else{
-	redis.hset('1', 'data', JSON.stringify(data));
-	redis.expire('1', 43200);
-	}	
-}
-
-exports.getMessage = function(data,err){ 
-   	if(err)
-    	console.log('Error in retriving data obj -> name:msg');
+module.exports.saveToDb = function(socketName, userName, message, callback){
+  redis.get(socketName, function(err, data){
+    if(!data){
+      data = {
+        messages: []
+      };
+    }
     else{
-
-    	redis.hget('1', 'data', function(err, data){
-  			if(err) throw err;
-  			else{
-    		console.log("name: "+ JSON.parse(data).name+ " msg: "+JSON.parse(data).msg);
-  			}
-		});
+      data = JSON.parse(data);
     }
 
-}
+    data.messages.push({userName: userName, message: message});
+    var multi = redis.multi();
+    
+    multi.set(socketName, JSON.stringify(data));
+    redis.expire('1', 43200);
+    multi.exec(callback);
+  });
+};
 
-exports.saveUsers = function(data,err){
-	if(err){
-		console.log('Error in pushing user name on to the DB');
-	}
-	else{
-		redis.lpush('usersInRoom',data.nick);
-	}
+module.exports.getMessage = function(socketName, callback){
+  redis.get(socketName, function(err, data){
+    if(err){
+      callback(err);
+    }
+    else{
+      if(data){
+        data = JSON.parse(data);
+        callback(null, data.messages);
+      }
+      else{
+        callback(null);
+      }
+    }
+  });
+};
+
+module.exports.saveUsers = function(data,err){
+  if(err){
+    console.log('Error in pushing user name on to the DB');
+  }
+  else{
+    redis.lpush('usersInRoom',data.nick);
+  }
 }
   
-exports.removeUsers = function(data,err){
-	if(err){
-		console.log('Error in deleting user name from the DB');
-	}
-	else{
-		redis.lrem('usersInRoom',data.nick);
-	}
+module.exports.removeUsers = function(data,err){
+  if(err){
+    console.log('Error in deleting user name from the DB');
+  }
+  else{
+    redis.lrem('usersInRoom',data.nick);
+  }
 }
